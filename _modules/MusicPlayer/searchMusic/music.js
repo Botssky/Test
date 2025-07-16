67ef4f3eb634bd27ae3f2580693b902d3e120ecd
Botssky <falsenewx@gmail.com>
2025-07-16 14:17:27 +0300

    Update music.js

--- _modules/MusicPlayer/searchMusic/music.js
+++ _modules/MusicPlayer/searchMusic/music.js
@@ -4,44 +4,41 @@ import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+import axios from "axios";
+
+const cache = {};
 
 export default async function searchMusic(string) {
-    const cache = JSON.parse(readFileSync(join(__dirname, "cache.json"), "utf8"));
     try {
         if (cache[string]) {
-            cache[string] = {
-                id: cache[string].id,
-                timestamp: new Date()
-            };
-        }
-        else {
+            cache[string].timestamp = new Date();
+            return cache[string].id;
+        } else {
             const response = await axios.get(
                 "https://youtube.googleapis.com/youtube/v3/search",
                 {
-                    params:
-                    {
+                    params: {
                         q: string,
                         channelType: "any",
                         type: "video",
-                        maxResults: "1",
-                        key: "AIzaSyBnzGmRG1FyITDirEenlEHkqzJgTVeHusE"
-                    }
+                        maxResults: 1,
+                        key: process.env.YOUTUBE_API_KEY,  // Lue avain ympäristömuuttujasta
+                    },
                 }
             );
 
-            cache[string] = {
-                id: response.data["items"][0]["id"]["videoId"],
-                timestamp: new Date()
-            };
-        }
+            if (!response.data.items || response.data.items.length === 0) {
+                throw new Error("Music not found");
+            }
 
-        return cache[string].id;
-    }
-    catch (error) {
+            cache[string] = {
+                id: response.data.items[0].id.videoId,
+                timestamp: new Date(),
+            };
+
+            return cache[string].id;
+        }
+    } catch (error) {
         throw new Error("Music not found");
-    }
-    finally {
-        writeFileSync(join(__dirname, "cache.json"), JSON.stringify(cache, null, 4));
     }
 }
